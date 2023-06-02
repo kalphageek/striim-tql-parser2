7698653c00247bacee5e7cf26b295a2db0670360
kalphageek <kalphageek@outlook.com>
2023-06-03 00:11:40 +0900

    comment

--- src/main/java/com/skhynix/datahub/striimtqlparser2/batch/ToTrailToJsonConfig.java
+++ src/main/java/com/skhynix/datahub/striimtqlparser2/batch/ToTrailToJsonConfig.java
@@ -8,4 +8,14 @@ public class ToTrailToJsonConfig {
      * 3.1 tql parser (open feign)
      * 4. writer
      */
+
+    /**
+     * tql parser
+     * 1. export tql
+     * 2.1. export tql parsing (return parsing dir)
+     * 2.1. compare old / new files -> 달라진 files parsing (return parsing result)
+     * 2.2. parsing files retrieve source/target tables -> striim cli (return retrieve data)
+     * 2.3. replace new files to old files
+     * 2.4. return parsing result (return parsing result)
+     */
 }
